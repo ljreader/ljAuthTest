4c3d8f6de32d879b44171b7aae50cb4279ad9aa8
ljreader <lawrencereader@outlook.com>
2018-01-22 20:07:32 -0700

    like button added

---
+++ script/google.js
@@ -0,0 +1,8 @@
+// https://github.com/ljreader/googleAuth.git
+// https://github.com/PurpleTreeUofU/upkeep
+// https://github.com/ljreader/ljAuthTest
+// https://ljreader.github.io/ljAuthTest/
+// https://github.com/PurpleTreeUofU/upkeep
+// https://PurpleTreeUofU.github.io/upkeep/
+
+
